1f1032ec655d41edc5176a79a5e6a91b68dc5ba6
Maxim Larin <maxim.larin@ximxim.com>
2018-01-09 18:18:30 +0300

    some new features and refactoring

--- Input/Clickable.cs
+++ Input/Clickable.cs
@@ -12,20 +12,37 @@ namespace Assets.Common.Scripts.Input
         public readonly EventWrap Clicked = new EventWrap();
         public readonly EventWrap MouseDown = new EventWrap();
         public readonly EventWrap MouseUp = new EventWrap();
+        public readonly EventWrap<string> ClickedId = new EventWrap<string>();
+        public readonly EventWrap<string> MouseDownId = new EventWrap<string>();
+        public readonly EventWrap<string> MouseUpId = new EventWrap<string>();
+
+        public string Id;
 
         public void OnPointerClick(PointerEventData eventData)
         {
-            if (InputController.InputIsEnabled) Clicked.Dispatch();
+            if (InputController.InputIsEnabled)
+            {
+                Clicked.Dispatch();
+                ClickedId.Dispatch(Id);
+            }
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
-            if (InputController.InputIsEnabled) MouseDown.Dispatch();
+            if (InputController.InputIsEnabled)
+            {
+                MouseDown.Dispatch();
+                MouseDownId.Dispatch(Id);
+            }
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
-            if (InputController.InputIsEnabled) MouseUp.Dispatch();
+            if (InputController.InputIsEnabled)
+            {
+                MouseUp.Dispatch();
+                MouseUpId.Dispatch(Id);
+            }
         }
     }
 }
